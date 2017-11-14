f5c5c78a1618ce6776043461ffa08ca06cdac1ab
Olga Kozlova <olgasergeevna0803@gmail.com>
2017-11-14 21:07:59 +0200

    generating of unique id for each object

---
+++ src/main/java/UniqueID.java
@@ -0,0 +1,8 @@
+public class UniqueID {
+
+    public static long generateID(Object object){
+        long currentTime = System.currentTimeMillis();
+        long hash = object.hashCode();
+        return currentTime+hash;
+    }
+}
